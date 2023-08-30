f854a30838b9b32613497696ec5501d6863785a9
David Dugue <ddugue@kumoweb.ca>
2023-08-30 10:34:55 -0400

    Deleted cache files

--- bin/binder/utils.py
+++ bin/binder/utils.py
@@ -1,24 +1,28 @@
 import unicodedata
 import re
 def next_or_none(iterator):
+    "Returns the next item of the iterator or None if no more items"
     try:
         return next(iterator)
     except StopIteration:
         return None
 
 def pop_or_none(l):
+    "Pop the first item of the list or None if the list is empty"
     try:
         return l.pop(0)
     except IndexError:
         return None
 
 def get_or_none(l, i):
+    "Returns the item at index 'i' in the 'l' list or None if absent"
     try:
         return l[i]
     except IndexError:
         return None
 
 def slugify(value):
+    "Normalize a string by replacing spaces. Useful to make stuff url friendlys"
     value.replace('.', '-').replace(':','-')
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
     value = re.sub('[^\w\s-]', '', value).strip().lower()
